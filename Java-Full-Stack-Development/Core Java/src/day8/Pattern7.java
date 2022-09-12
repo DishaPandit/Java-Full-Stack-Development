a9c98bc3ab2c3ac9717b45c1921a2ec154704560
DISHA <71927399+DishaPandit@users.noreply.github.com>
2022-09-12 22:52:29 +0530

    Add files via upload

---
+++ Java-Full-Stack-Development/Core Java/src/day8/Pattern7.java
@@ -0,0 +1,21 @@
+package day8;
+
+public class Pattern7 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=6;
+		for(int i=0;i<n;i++)
+		{
+				
+			for(int j=0;j<n;j++)
+			{
+					
+	              System.out.print(j);
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
