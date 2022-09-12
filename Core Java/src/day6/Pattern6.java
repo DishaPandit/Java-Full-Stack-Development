b30e326a17edc3ee4cedcab29501732f29536f38
DISHA <71927399+DishaPandit@users.noreply.github.com>
2022-09-12 22:58:27 +0530

    Add files via upload

---
+++ Core Java/src/day6/Pattern6.java
@@ -0,0 +1,17 @@
+package day6;
+
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int n=6;	
+		for(int i=0;i<n;i++)
+		{
+			
+			System.out.print("*");
+		}
+	}
+}
