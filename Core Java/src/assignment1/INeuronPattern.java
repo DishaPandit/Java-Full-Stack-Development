48ff04917a66bc03826cba0f999d34a3384cb79a
Disha Pandit <dishapandit@1998@gmail.com>
2022-09-19 21:15:27 +0530

    Assignment1

---
+++ Core Java/src/assignment1/INeuronPattern.java
@@ -0,0 +1,42 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class INeuronPattern {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = sc.nextInt();
+		for(int i = 1; i<=n; i++)
+		{
+			//Print I
+			for(int j=1; j<=1; j++)
+			{
+				System.out.print("*");
+			}
+			//System.out.println();
+			System.out.println(" ");
+			
+			//Print N
+			for(int j=1; j<=1; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			
+			//Print E
+			
+			//Print U
+			
+			//Print R
+			
+			//Print O
+			
+			//Print N
+		}
+
+	}
+
+}
